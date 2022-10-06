ffb18ebecbec1c2f819b4e399691066ecc6a883b
xiewenrong <xiewenrong@bytedance.com>
2022-10-06 22:53:13 +0800

    [feat] init remote rep

---
+++ content.js
@@ -0,0 +1,9 @@
+
+
+var roomPanel = document.createElement("div");
+roomPanel.setAttribute("id", "roomPanel");
+roomPanel.innerHTML = "<div class='roomPanelTitle'> 房间号: 998176 </div> <hr/>\
+                       <div class='roomPanelUser'> linsen </div>";
+
+
+document.body.parentNode.appendChild(roomPanel);
